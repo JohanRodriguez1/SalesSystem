4ba07d7b47d5396fe4a031b502699dab68283494
jmro0 <jmro0@EQUIPOJOHAN>
2021-08-11 17:06:21 -0500

    Agregar archivos de proyecto.

---
+++ SalesSystem/Areas/Users/Pages/Account/Register.cshtml.cs
@@ -0,0 +1,53 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using SalesSystem.Areas.Users.Models;
+using SalesSystem.Library;
+
+namespace SalesSystem.Areas.Users.Pages.Account
+{
+    public class RegisterModel : PageModel
+    {
+        private SignInManager<IdentityUser> _signInManager;
+        private UserManager<IdentityUser> _userManager;
+        private RoleManager<IdentityRole> _roleManager;
+        private LUsersRoles _usersRole;
+
+        public RegisterModel(
+            SignInManager<IdentityUser> signInManager,
+            UserManager<IdentityUser> userManager,
+            RoleManager<IdentityRole> roleManager)
+        {
+            _signInManager = signInManager;
+            _userManager = userManager;
+            _roleManager = roleManager;
+            _usersRole = new LUsersRoles();
+        }
+
+        public void OnGet()
+        {
+            Input = new InputModel
+            {
+                rolesLista = _usersRole.getRoles(_roleManager)
+            };
+        }
+
+        [BindProperty]
+        public InputModel Input { get; set; }
+
+        public class InputModel : InputModelRegister
+        {
+            public IFormFile AvatarImage { get; set; }
+
+            public string ErrorMessage { get; set; }
+
+            public List<SelectListItem> rolesLista { get; set; }
+        }
+    }
+}
