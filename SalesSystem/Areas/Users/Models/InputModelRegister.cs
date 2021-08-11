31ed8077b870490d5f36895ec30672ab72b1ca70
jmro0 <jmro0@EQUIPOJOHAN>
2021-08-11 17:06:21 -0500

    Agregando archivos de proyecto.

---
+++ SalesSystem/Areas/Users/Models/InputModelRegister.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SalesSystem.Areas.Users.Models
+{
+    public class InputModelRegister
+    {
+        [Required(ErrorMessage = "The first name is required.")]
+        public string FirstName { get; set; }
+
+        [Required(ErrorMessage = "The last name is required.")]
+        public string LastName { get; set; }
+
+        [Required(ErrorMessage = "The ID is required.")]
+        public string ID { get; set; }
+
+        [Required(ErrorMessage = "The phone number is required.")]
+        [DataType(DataType.PhoneNumber)]
+        [RegularExpression(@"^\(?([0-9]{2})\)?[-. ]?([0-9]{3})[-. ]?([0-9]{7})$", ErrorMessage = "The entered phone format is invalid.")]
+        public string PhoneNumber { get; set; }
+
+        [Required(ErrorMessage = "The email is required.")]
+        [EmailAddress(ErrorMessage = "The entered email format is invalid.")]
+        public string Email { get; set; }
+
+        [Display(Name = "Password")]
+        [Required(ErrorMessage = "The password is required.")]
+        [StringLength(10, ErrorMessage = "The number of characters in {0} must be at least {2}", MinimumLength = 5)]
+        public string Password { get; set; }
+
+        [Required]
+        public string Role { get; set; }
+    }
+}
---
+++ SalesSystem/Controllers/HomeController.cs
@@ -0,0 +1,57 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using SalesSystem.Models;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SalesSystem.Controllers
+{
+    public class HomeController : Controller
+    {
+        //IF YOU ARE GOING TO REGISTER MORE ROLES ENABLE THE NEXT PROCEDURE FOR THEIR EXECUTION.
+        //IServiceProvider _serviceProvider;
+
+        public HomeController(IServiceProvider serviceProvider)
+        {
+            //IF YOU ARE GOING TO REGISTER MORE ROLES ENABLE THE NEXT PROCEDURE FOR THEIR EXECUTION.
+            //_serviceProvider = serviceProvider;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            //IF YOU ARE GOING TO REGISTER MORE ROLES ENABLE THE NEXT PROCEDURE FOR THEIR EXECUTION.
+            //await CreateRolesAsync(_serviceProvider);
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+
+        private async Task CreateRolesAsync(IServiceProvider serviceProvider)
+        {
+            var roleManager = serviceProvider.GetRequiredService<RoleManager<IdentityRole>>();
+            String[] rolesName = { "Admin", "User" };
+            foreach (var item in rolesName)
+            {
+                var roleExist = await roleManager.RoleExistsAsync(item);
+                if (!roleExist)
+                {
+                    await roleManager.CreateAsync(new IdentityRole(item));
+                }
+            }
+        }
+    }
+}
