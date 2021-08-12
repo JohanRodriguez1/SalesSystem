f305cf3908b4320d45b3ac94b5d9d0ab608d8d89
Johan Rodriguez <jmro0@hotmail.com>
2021-08-11 21:37:38 -0500

    agregando

--- SalesSystem/Areas/Users/Pages/Account/Register.cshtml.cs
+++ SalesSystem/Areas/Users/Pages/Account/Register.cshtml.cs
@@ -2,11 +2,13 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
 using SalesSystem.Areas.Users.Models;
 using SalesSystem.Data;
 using SalesSystem.Library;
@@ -21,18 +23,23 @@ namespace SalesSystem.Areas.Users.Pages.Account
         private LUsersRoles _usersRole;
         public static InputModel _dataInput;
         private ApplicationDbContext _context;
+        private IWebHostEnvironment _environment;
+        private Uploadimage _uploadimage;
 
         public RegisterModel(
             SignInManager<IdentityUser> signInManager,
             UserManager<IdentityUser> userManager,
             RoleManager<IdentityRole> roleManager,
-            ApplicationDbContext context)
+            ApplicationDbContext context,
+            IWebHostEnvironment environment)
         {
             _signInManager = signInManager;
             _userManager = userManager;
             _roleManager = roleManager;
             _usersRole = new LUsersRoles();
             _context = context;
+            _environment = environment;
+            _uploadimage = new Uploadimage();
         }
 
         public void OnGet()
@@ -86,6 +93,41 @@ namespace SalesSystem.Areas.Users.Pages.Account
                 if (userList.Count.Equals(0))
                 {
                     var strategy = _context.Database.CreateExecutionStrategy();
+                    await strategy.ExecuteAsync(async () => {
+                        using (var transaction = _context.Database.BeginTransaction())
+                        {
+                            try
+                            {
+                                var user = new IdentityUser
+                                {
+                                    UserName = Input.Email,
+                                    Email = Input.Email,
+                                    PhoneNumber = Input.PhoneNumber
+                                };
+                                var result = await _userManager.CreateAsync(user, Input.Password);
+                                if (result.Succeeded)
+                                {
+                                    await _userManager.AddToRoleAsync(user, Input.Role);
+                                    var dataUser = _userManager.Users.Where(u => u.Email.Equals(Input.Email)).ToList().Last();
+                                    var imageByte = await _uploadimage.ByteAvatarImageAsync(Input.AvatarImage, _environment);
+                                }
+                                else
+                                {
+                                    foreach (var item in result.Errors) 
+                                    {
+                                        _dataInput.ErrorMessage = item.Description;
+                                    }
+                                    valor = false;
+                                }
+                            }
+                            catch (Exception ex)
+                            {
+                                _dataInput.ErrorMessage = ex.Message;
+                                transaction.Rollback();
+                                valor = false;
+                            }
+                        }
+                    });
                 }
                 else
                 {
